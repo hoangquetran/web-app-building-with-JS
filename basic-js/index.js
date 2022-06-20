5213c0cf7da676c3c0d923c21f7aacedd19ea52c
Que Tran <hoanquexttx@gmail.com>
2022-06-20 18:03:35 +0700

    validate email

--- basic-js/index.js
+++ basic-js/index.js
@@ -17,17 +17,42 @@
 
 // check Zipcode
 
-let regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
+// let regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
 
-function isUSZipCode(zipcode) {
-    if(regex.test(zipcode)) {
-        return true;
+// function isUSZipCode(zipcode) {
+//     if(regex.test(zipcode)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// var zipcode = "82941";
+// console.log(isUSZipCode(zipcode));
+
+// zipcode = "0320";
+// console.log(isUSZipCode(zipcode));
+
+// Validate email
+
+let regex = /^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
+
+function isValidateEmail(str) {
+    if(regex.test(str)) {
+        console.log("Invalid email address.");
+    }else {
+        console.log("Please enter a valid email address.");
     }
-    return false;
 }
 
-var zipcode = "82941";
-console.log(isUSZipCode(zipcode));
+let listEmail = [
+    "a@gmail.com",
+    "@gmail.com",
+    "ab@yahoo.com",
+    "ab@gmail."
+]
 
-zipcode = "0320";
-console.log(isUSZipCode(zipcode));
+for (var i = 0; i < listEmail.length; i++) {
+    console.log(listEmail[i]);
+    isValidateEmail(listEmail[i]);
+}
+  
