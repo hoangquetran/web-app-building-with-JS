e17b374c5d094523a1754b68be0191f02b85b92b
Que Tran <hoanquexttx@gmail.com>
2022-06-20 17:10:51 +0700

    check US ZipCode

--- basic-js/index.js
+++ basic-js/index.js
@@ -15,3 +15,18 @@
 // isFirstLetterUpperCase("dAbd");
 
 // check Zipcode
+
+let regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
+
+function isUSZipCode(zipcode) {
+    if(regex.test(zipcode)) {
+        return true;
+    }
+    return false;
+}
+
+var zipcode = "82941";
+console.log(isUSZipCode(zipcode));
+
+zipcode = "0320";
+console.log(isUSZipCode(zipcode));
