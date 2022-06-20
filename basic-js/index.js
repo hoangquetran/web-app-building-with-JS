899482b3c283cccc1597aab3ac00fd939fa7cbf2
Que Tran <hoanquexttx@gmail.com>
2022-06-20 18:22:28 +0700

    validate account

--- basic-js/index.js
+++ basic-js/index.js
@@ -2,57 +2,26 @@
 
 // logger("Xin chào");
 
-// let regex = /^[A -Z]/;
+let regex = /^[_a-z0-9]{6,}$/;
 
-// function isFirstLetterUpperCase(str) {
-//     if(regex.test(str)) {
-//         console.log("The first letter is uppercase");
-//     } else {
-//         console.log("The first letter is not uppercase");
-//     }
-// }
-
-// isFirstLetterUpperCase("dAbd");
-
-
-// check Zipcode
-
-// let regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
-
-// function isUSZipCode(zipcode) {
-//     if(regex.test(zipcode)) {
-//         return true;
-//     }
-//     return false;
-// }
-
-// var zipcode = "82941";
-// console.log(isUSZipCode(zipcode));
-
-// zipcode = "0320";
-// console.log(isUSZipCode(zipcode));
-
-// Validate email
-
-let regex = /^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
-
-function isValidateEmail(str) {
+function isValidateAccount(str) {
     if(regex.test(str)) {
-        console.log("Invalid email address.");
+        console.log("Invalid account.");
     }else {
-        console.log("Please enter a valid email address.");
+        console.log("Please enter a valid account.");
     }
 }
 
-let listEmail = [
-    "a@gmail.com",
-    "@gmail.com",
-    "ab@yahoo.com",
-    "ab@gmail."
+let listAcc = [
+    "123abc_",
+    "_abc123",
+    ".@",
+    "ab______",
+    "abcde "
 ]
 
-for (var i = 0; i < listEmail.length; i++) {
-    console.log(listEmail[i]);
-    isValidateEmail(listEmail[i]);
+for (var i = 0; i < listAcc.length; i++) {
+    console.log(listAcc[i]);
+    isValidateAccount(listAcc[i]);
 }
   
